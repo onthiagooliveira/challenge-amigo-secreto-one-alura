ba6fd89509785dfa2ef5fb75086e1a9b84b93346
Thiago Oliveira <onthiagooliveira@outlook.com>
2025-08-15 20:06:26 -0300

    Created schedule DOM and importing datas of input

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+// Manipulando o DOM
+const formulario = document.getElementById('form-input-amigo');
+const btnAdd = document.getElementsByClassName('button-add');
+
+formulario.addEventListener('submit', function(evento) {
+  evento.preventDefault();
+
+  const inputUser = document.getElementById('amigo').value;
+
+  validarEntrada(inputUser);
+})
+
+
+
+// Lista de amigos à sortear
+let listaAmigosSecretos = [];
+
+
+// Função que valida os dados de input
+function validarEntrada(val) {
+  
+}
+
+// Função que adiciona um amigo a lista
+function adicionarAmigo() {
+
+}
+// Função que sorteia um amigo com base na lista de amigos inseridos
