99bc69fa27b53d87414f0f7b1ad1982051cbb729
Thiago Oliveira <onthiagooliveira@outlook.com>
2025-08-15 23:22:58 -0300

    Created error mesage when there is no input value

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 // Manipulando o DOM
 const formulario = document.getElementById("form-input-amigo");
-const btnAdd = document.getElementsByClassName("button-add");
+let listarEmTela = document.getElementById("listaAmigos");
 const resultado = document.getElementById("resultado");
+
 
 formulario.addEventListener("submit", function (evento) {
   evento.preventDefault();
@@ -13,13 +14,50 @@ formulario.addEventListener("submit", function (evento) {
 // Lista de amigos à sortear
 let listaAmigosSecretos = [];
 
-// Função que valida os dados de input
+
+// Verificando os dados de input
 function validarEntrada(val) {
   return Boolean(val);
 }
 
-// Função que adiciona um amigo a lista
-function adicionarAmigo() {
-  
+
+// Manipulação de strings
+
+function errorMensagem(section) {
+  // Adiciona um icone e limpa mensagens antigas
+  section.innerHTML = `
+  <img width="24" height="24"
+  src="https://img.icons8.com/material-outlined/24/error--v1.png" 
+  alt="error--v1"/>`;
+
+  const p = document.createElement("p");
+  p.textContent = "Informe um nome válido.";
+  section.append(p);
+
+  // adiciona a classe para exibir
+  section.classList.add("show");
+
+  // remove automaticamente depois de 3 segundos
+  setTimeout(() => {
+    section.classList.remove("show");
+  }, 3000);
 }
+
+
+
+// Adiciona um amigo a lista
+function adicionarAmigo() {
+  let inputUser = document.getElementById("amigo").value;
+  let status = validarEntrada(inputUser);
+
+  if (status) {
+    const novoAmigo = inputUser;
+    listaAmigosSecretos.push(novoAmigo);
+    console.log(listaAmigosSecretos);
+  } else {
+    let msgTag = document.getElementById("message");
+    errorMensagem(msgTag);
+  }
+}
+
 // Função que sorteia um amigo com base na lista de amigos inseridos
