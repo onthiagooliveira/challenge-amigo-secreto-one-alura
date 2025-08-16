cbb2b763e3cb50072974486a34e69af4b8978666
Thiago Oliveira <onthiagooliveira@outlook.com>
2025-08-15 21:20:50 -0300

    Created function that validate data of input

--- app.js
+++ app.js
@@ -1,28 +1,25 @@
 // Manipulando o DOM
-const formulario = document.getElementById('form-input-amigo');
-const btnAdd = document.getElementsByClassName('button-add');
+const formulario = document.getElementById("form-input-amigo");
+const btnAdd = document.getElementsByClassName("button-add");
+const resultado = document.getElementById("resultado");
 
-formulario.addEventListener('submit', function(evento) {
+formulario.addEventListener("submit", function (evento) {
   evento.preventDefault();
 
-  const inputUser = document.getElementById('amigo').value;
+  adicionarAmigo();
 
-  validarEntrada(inputUser);
-})
-
-
+});
 
 // Lista de amigos à sortear
 let listaAmigosSecretos = [];
 
-
 // Função que valida os dados de input
 function validarEntrada(val) {
-  
+  return Boolean(val);
 }
 
 // Função que adiciona um amigo a lista
 function adicionarAmigo() {
-
+  
 }
 // Função que sorteia um amigo com base na lista de amigos inseridos
